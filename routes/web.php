190cd05d4154df192aff28f111564fb78449b7c0
Golam Rahman Sagor <grsagor08@gmail.com>
2024-12-05 01:43:21 +0600

    initial setup of the template done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\Frontend\FrontendController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(FrontendController::class)->group(function() {
+    Route::get('/', 'index')->name('front.index');
 });
