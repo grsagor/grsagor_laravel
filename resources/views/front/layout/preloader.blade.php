9d901285426d4164676a7da552c41024cde2b54c
Golam Rahman Sagor <grsagor08@gmail.com>
2025-08-27 07:55:59 +0600

    git ignore

--- resources/views/front/layout/preloader.blade.php
+++ resources/views/front/layout/preloader.blade.php
@@ -18,14 +18,14 @@
     class="position-fixed top-0 start-0 w-100 vh-100 bg-white d-flex justify-content-center align-items-center"
     style="z-index: 1050;">
     <div class="d-flex gap-3">
-        <div
-            style="width: 15px; height: 15px; background-color: #0d6efd; border-radius: 50%; animation: bounce 1.4s infinite ease-in-out both;">
+        <div class="bg-primary"
+            style="width: 15px; height: 15px; border-radius: 50%; animation: bounce 1.4s infinite ease-in-out both;">
         </div>
-        <div
-            style="width: 15px; height: 15px; background-color: #0d6efd; border-radius: 50%; animation: bounce 1.4s infinite ease-in-out both; animation-delay: 0.2s;">
+        <div class="bg-primary"
+            style="width: 15px; height: 15px; border-radius: 50%; animation: bounce 1.4s infinite ease-in-out both; animation-delay: 0.2s;">
         </div>
-        <div
-            style="width: 15px; height: 15px; background-color: #0d6efd; border-radius: 50%; animation: bounce 1.4s infinite ease-in-out both; animation-delay: 0.4s;">
+        <div class="bg-primary"
+            style="width: 15px; height: 15px; border-radius: 50%; animation: bounce 1.4s infinite ease-in-out both; animation-delay: 0.4s;">
         </div>
     </div>
 </div>
