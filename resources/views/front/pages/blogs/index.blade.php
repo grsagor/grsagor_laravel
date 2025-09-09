0eb7690b4a46d7d554d0d640feeb9faf65e8016c
Golam Rahman Sagor <grsagor08@gmail.com>
2025-09-09 07:57:02 +0600

    Update index.blade.php

--- resources/views/front/pages/blogs/index.blade.php
+++ resources/views/front/pages/blogs/index.blade.php
@@ -1,4 +1,4 @@
-{{-- @extends('front.layout.app')
+@extends('front.layout.app')
 
 @section('title', 'Blog')
 
@@ -6,50 +6,4 @@
     <div class="py-5">
         @include('front.pages.home.sections.blogs')
     </div>
-@endsection --}}
-
-
-
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" />
-</head>
-
-<body>
-    <div class="row">
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <h1>Hello, world!</h1>
-        </div>
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <h1>Hello, world!</h1>
-        </div>
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <h1>Hello, world!</h1>
-        </div>
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <h1>Hello, world!</h1>
-        </div>
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <h1>Hello, world!</h1>
-        </div>
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <h1>Hello, world!</h1>
-        </div>
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <h1>Hello, world!</h1>
-        </div>
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <h1>Hello, world!</h1>
-        </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+@endsection
